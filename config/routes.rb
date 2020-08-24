82aeea7d582c544eb3788d9917593d33c2effc47
e-mbrown <66039494+e-mbrown@users.noreply.github.com>
2020-08-24 15:08:59 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    resource :users, only: [:create]
+  
+  resources :comments do
+
+  end  
+  resource :users, only: [:create]
     post "/login", to: "users#login"
     get "/auto_login", to: "users#auto_login"
     resource :activity do
 
-    end
   end
 end
