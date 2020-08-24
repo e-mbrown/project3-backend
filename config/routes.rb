28be7119edd25c44eb979f29b5a536df670de005
e-mbrown <ebsonari@gmail.com>
2020-08-24 13:59:03 -0400

    Started with comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :comments
   Rails.application.routes.draw do
+  resources :comments
     resource :users, only: [:create]
     post "/login", to: "users#login"
     get "/auto_login", to: "users#auto_login"
