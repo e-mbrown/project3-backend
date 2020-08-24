bff069f730f643e8dbae2bf55295bea2ed709a49
donovanrichardson <donovanspqa@gmail.com>
2020-08-24 15:39:37 -0400

    fixing migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
   resources :comments do
 
   end
 
-    resource :users, only: [:create]
-=======
-  
-  resources :comments do
-
-  end  
   resource :users, only: [:create]
->>>>>>> 82aeea7d582c544eb3788d9917593d33c2effc47
-    post "/login", to: "users#login"
-    get "/auto_login", to: "users#auto_login"
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
 
-    resource :activity do
+  resource :activity do
 
   end
+
+end
 
