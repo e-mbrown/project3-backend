0c6378b328b74cd2e7eb85aa724367c4368f14c3
Claudia <claudias.mazariegos@gmail.com>
2020-08-24 15:03:33 -0400

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resource :users, only: [:create]
     post "/login", to: "users#login"
     get "/auto_login", to: "users#auto_login"
+    resource :activity do
   end
 end
