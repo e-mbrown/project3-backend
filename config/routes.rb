3afbabf45cb28e79a6018e955bd0a5e852cb896b
NARISSA <nkh239@nyu.edu>
2020-08-25 10:01:52 -0400

    Added route by location

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get "/activities", to: "activity#index"
   get "/activities/:id", to: "activity#show"
+  get "/activities/q/:location", to: "activity#location"
   put "/activities/:id", to: "activity#update"
   delete "/activities/:id", to: "activity#destroy"
   post "/activities", to: "activity#create"
